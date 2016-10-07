8b65a1f68e306d0ef23bb976fb1ba292dd98b92e
Charley Lawton <user@CODECLAN055.local>
2016-10-07 12:09:22 +0100

    api started

--- bone_api/boneApi.js
+++ bone_api/boneApi.js
@@ -1,2 +1,8 @@
-// var BoneApi = function
 // this will contain restful routes
+var BoneApi = function(app){
+  var bones = require('../client/src/models/bones')();
+
+  var Bone= require('../client/src/models/bone');
+
+  
+}

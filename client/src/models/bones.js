826fc31c36b53f33237ec1de3e7c8182d6222c48
Emma Boardman <emma@emmaboardman.co.uk>
2016-10-07 11:45:11 +0100

    skull data added

--- client/src/models/bones.js
+++ client/src/models/bones.js
@@ -1,1 +1,18 @@
-// this will be something. 
+var Bone = require('./bone');
+
+var Bones = function(){
+
+  var bone1 = new Bone({
+    name: "Skull",
+    fact: "This part of the body protects your brain",
+    image: 
+  });
+
+  var bone2 = new Bone({
+    name: 
+    fact: 
+    image: 
+  })
+
+
+}

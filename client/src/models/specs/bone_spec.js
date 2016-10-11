a37c79c7ce913ee1e2d9dceb7d98802c5bb1f2e3
srobertson90 <stuart.robertson.applications@gmail.com>
2016-10-11 09:56:22 +0100

    added bone fact test, passing as expected

--- client/src/models/specs/bone_spec.js
+++ client/src/models/specs/bone_spec.js
@@ -3,17 +3,22 @@ var Bone = require('../bone');
 
 describe('Bone', function(){
 
-var testBone;
+  var testBone;
 
-before(function(){
-  testBone = new Bone({
-    name:"Test Bone", 
-    fact:"The test bone is connected to the... other test bone.",
-    image: "nope.jpg"});
-});
+  before(function(){
+    testBone = new Bone({
+      name:"Test Bone", 
+      fact:"The test bone is connected to the... other test bone.",
+      image: "nope.jpg"});
+  });
 
-it('should have a name', function(){
-  assert.equal("Test Bone", testBone.name);
-});
+  it('should have a name', function(){
+    assert.equal("Test Bone", testBone.name);
+  });
+
+  it('should have a fact', function(){
+    assert.equal("The test bone is connected to the... other test bone.", testBone.fact);
+  });
+
 
 });

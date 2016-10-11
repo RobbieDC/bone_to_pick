c172ba731fd2e31545d82d86f09a48e6dd9607cc
Charley Lawton <charlotte.lawton@googlemail.com>
2016-10-11 15:49:36 +0100

    mouth added to digestive api

--- client/src/models/digestives.js
+++ client/src/models/digestives.js
@@ -3,9 +3,9 @@ var Digestive = require("./digestive");
 var Digestives = function() {
 
   var digestive1 = new Digestive({
-    name: "",
-    fact: "",
-    image: ""
+    name: "mouth",
+    fact: "An average human has 32 teeth",
+    image: "./public/images/mouth.jpg"
   });
 
   var digestive2 = new Digestive({

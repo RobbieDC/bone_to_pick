a358f2aaaa69921c97730d283a973667a6dbb806
srobertson90 <stuart.robertson.applications@gmail.com>
2016-10-11 09:51:42 +0100

    defined test bone

--- client/src/models/specs/bone_spec.js
+++ client/src/models/specs/bone_spec.js
@@ -3,6 +3,15 @@ var Bone = require('../bone');
 
 describe('Bone', function(){
 
+var testBone;
+
+before(function(){
+  testBone = new Bone({
+    name:"Test Bone", 
+    fact:"The test bone is connected to the... other test bone.",
+    image: "nope.jpg"});
+});
+
 
 
 });

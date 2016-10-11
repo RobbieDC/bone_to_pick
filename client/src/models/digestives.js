e59b10415956a4c375a6b37db1d61926f8836ded
Charley Lawton <charlotte.lawton@googlemail.com>
2016-10-11 15:59:20 +0100

    oespahgus and liver added

--- client/src/models/digestives.js
+++ client/src/models/digestives.js
@@ -3,16 +3,30 @@ var Digestive = require("./digestive");
 var Digestives = function() {
 
   var digestive1 = new Digestive({
-    name: "mouth",
+    name: "Mouth",
     fact: "An average human has 32 teeth",
     image: "./public/images/mouth.jpg"
   });
 
   var digestive2 = new Digestive({
-    name: "",
+    name: "Esophagus",
+    fact: "The esophagus is around 9-10 inches long, and connects your mouth to  your stomach",
+    image: ""
+  });
+
+  var digestive3 = new Digestive({
+    name: "Liver",
+    fact: "The liver is able to regenerate dead or damaged tissue",
+    image: ""
+  });
+
+  var digestive = new Digestive({
+    name: "Liver",
     fact: "",
     image: ""
   });
+
+
 
   return [digestive1, digestive2]
 

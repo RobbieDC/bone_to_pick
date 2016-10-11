ff6603447781209d3b5937b5639e05fb0854b80b
Robbie Campbell <rcampbell182@gmail.com>
2016-10-11 15:47:38 +0100

    tidied bonesApi and added digestivesApi

--- bone_api/boneApi.js
+++ bone_api/boneApi.js
@@ -1,10 +1,6 @@
-// this will contain restful routes
 var BoneApi = function(app){
-  var bones = require('../client/src/models/bones')();
+  var bones = require('../client/src/models/bones');
 
-  var Bone= require('../client/src/models/bone');
-
-  //film index
   app.get('/api/bones', function(req, res){
     res.json(bones);
   });

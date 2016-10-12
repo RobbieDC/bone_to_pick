d730d2fc6e5a562d35d31321106b01306631031a
Robbie Campbell <rcampbell182@gmail.com>
2016-10-12 10:40:55 +0100

    created body constructor

--- bone_api/boneApi.js
+++ bone_api/boneApi.js
@@ -1,12 +1,8 @@
 var BoneApi = function(app){
   var bones = require('../client/src/models/bones')();
-  var muscles = require('../client/src/models/muscles')();
-  var digestives = require('../client/src/models/digestives')();
 
   app.get('/api/bones', function(req, res){
     res.json(bones);
-    res.json(muscles);
-    res.json(digestives);
   });
 
 }
---
+++ client/src/models/body.js
@@ -0,0 +1,8 @@
+var Body = function(options) {
+  this.bones = options.bones;
+  this.muscles = options.muscles;
+  this.digestives = options.digestives;
+}
+
+module.exports = Body;
+
